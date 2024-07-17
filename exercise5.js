f9237d7d07057726ab8310c367f9160aaa6297c0
oyangferreira <yang.ferreira2023@gmail.com>
2024-07-17 02:11:47 -0300

    exercises

--- exercise5.js
+++
@@ -1,3 +0,0 @@
-for(let n = 1; n <= 10; n++) {
-    console.log(n);
-}
--- exercise7.js
+++
@@ -1,5 +0,0 @@
-const numbers = [1, 2, 3, 4, 5];
-
-for(let n = 0; n < numbers.length; n++) {
-    console.log(numbers[n]);
-}
