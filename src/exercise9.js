b6f08fdacfa7a0d67bec94360f0239787015ab35
oyangferreira <yang.ferreira2023@gmail.com>
2024-07-17 02:21:43 -0300

    invert string

--- src/exercise9.js
+++ src/exercise9.js
@@ -8,4 +8,4 @@ let originString = readline.question("Enter the text:");
 let stringInverted = InvertString(originString);
 
 console.log("Origin String:", originString);
-console.log("String Inverted:", stringInverted);
+console.log("String Inverted:", stringInverted); 
